651c0d1de481338c986118da34332476b50479bf
antoine_quesnel <antoinequesnel11@gmail.com>
2023-03-15 21:47:30 -0400

    Changed myscript to execute on automated workflow on git

--- myscript.py
+++ myscript.py
@@ -1,6 +1,4 @@
 import os
 import sys
-badHash = sys.argv[1]
-goodHash = sys.argv[2]
-os.system(f'git bisect start {badHash} {goodHash}')
+os.system(f'git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect run python manage.py test')
