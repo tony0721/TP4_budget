9e809e7fbb50eef76baa064c5a3b656e46f8a659
antoine_quesnel <antoinequesnel11@gmail.com>
2023-03-15 22:07:15 -0400

    Trying to add reset, it seems he doesnt take the inputs as exepected altough locally it works

--- myscript.py
+++ myscript.py
@@ -4,3 +4,4 @@ badHash = "c1a4be04b972b6c17db242fc37752ad517c29402"
 goodHash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 os.system(f'git bisect start {badHash} {goodHash}')
 os.system('git bisect run python manage.py test')
+os.system('git bisect reset')
